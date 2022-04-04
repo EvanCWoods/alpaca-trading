2ef4a146bd7281f60a60a34caf4cd8927ff6a22e
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-04-04 17:35:43 +1000

    added bollinger bands to tests.py

--- tests.py
+++ tests.py
@@ -1,6 +1,8 @@
+from cProfile import label
 import csv
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 # get the data from the CSV file
 def getData():
@@ -26,33 +28,39 @@ def getData():
   return rows
 
 
-# Average the data
-def average():
+# get the close data
+def getClose():
   close = []
 
   data = getData()
   for element in data:
     close.append(element.get("Close"))
 
-    x = np.arange(0, len(data))
+  return pd.DataFrame(close)
 
+ 
+#  Get the average of a given window
+def getAverage(windowSize):
+  data = getClose()
+  x = np.arange(0, len(data))
+  moving_averages = []
 
-  window_size = 10
-  
-  i = 0
-  # Initialize an empty list to store moving averages
-  moving_averages = [0,0,0,0,0,0,0,0,0]
-  
+  n = 0
+  while n < windowSize:
+    moving_averages.append(0)
+    n+=1
+
+  i = 0  
   # Loop through the array to consider
   # every window of size 3
-  while i < len(close) - window_size + 1:
+  while i < len(data) - windowSize + 1:
     
     # Store elements from i to i+window_size
     # in list to get the current window
-    window = close[i : i + window_size]
+    window = data[i : i + windowSize]
   
     # Calculate the average of current window
-    window_average = round(sum(window) / window_size, 2)
+    window_average = round(sum(window) / windowSize, 2)
       
     # Store the average of current
     # window in moving average list
@@ -61,16 +69,17 @@ def average():
     # Shift window to right by one position
     i += 1
 
-  # showData(x, close, moving_averages)
   return moving_averages
   
 
 # Show the data
-def showData(x,y, average=None):
-  if (average != None):
+def showData(x,y, average1=None, average2=None):
+  if (average1 != None or average2 != None):
     plt.figure(figsize=(15, 8))
-    plt.plot(x, y)
-    plt.plot(x, average)
+    plt.plot(x[-300:], y[-300:], label="Close")
+    plt.plot(x[-300:], average1[-300:], label=" 10 day Average")
+    plt.plot(x[-300:], average2[-300:], label=" 5 day Average")
+    plt.legend(loc="upper left")
     plt.show()
   else:
     plt.figure(figsize=(15, 8))
@@ -79,8 +88,37 @@ def showData(x,y, average=None):
   
 
 
+
+
+def get_sma(prices, rate):
+    return prices.rolling(rate).mean()
+
+def get_bollinger_bands(prices, rate=20):
+    sma = get_sma(prices, rate)
+    std = prices.rolling(rate).std()
+    bollinger_up = sma + std * 2 # Calculate top band
+    bollinger_down = sma - std * 2 # Calculate bottom band
+    return bollinger_up, bollinger_down
+
+
+bollinger_up, bollinger_down = get_bollinger_bands(getClose())
+
+print(len(bollinger_up), len(bollinger_down))
+
+plt.title("BTC" + ' Bollinger Bands')
+plt.xlabel('Days')
+plt.ylabel('Closing Prices')
+plt.plot(getClose()[-300:], label='Closing Prices')
+plt.plot(bollinger_up[-300:], label='Bollinger Up', c='g')
+plt.plot(bollinger_down[-300:], label='Bollinger Down', c='g')
+plt.legend()
+plt.show()
+
+
 def main():
-  average()
+  y = getClose()
+  x = np.arange(0, len(y))
+  # showData(x, y, bollinger_down, bollinger_up)
 
 
 if (__name__ == "__main__"):
