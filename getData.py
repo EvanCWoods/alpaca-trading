719c5d3f366c2c84c6d0113790d017564d298132
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-04-13 21:18:00 +1000

    fully integrated the new api to the script and tested it

--- getData.py
+++ getData.py
@@ -1,64 +1,47 @@
-# import time
-# import requests
-# from decouple import config
-
-# # GLOBAL VARIABLES
-# HOUR = 3600
-# API_ENDPOINT = config("API_ENDPOINT")
-
-
-# # Function to get the data from the api
-# def getDataFeed():
-#     data = requests.get(API_ENDPOINT)
-#     return data.json()
-
-
-# # Recursive function to get the current unix timestamp
-# def getHours():
-#     currentTime = int(time.time())  # Get the current time as an interger
-
-#     if (currentTime % HOUR == 0):   # If the time is devisable by the number of seconds in an hour, it is a new hour
-#         print(getDataFeed())    # Print the data form the api
-#         time.sleep(1)
-#         getHours()
-#     else:   # If the current time is not a new hour, re run the function, adding a 1 second delay
-#         time.sleep(1)
-#         getHours()
-
-
-# getHours()
-
-
+import time
+from decouple import config
 from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
-import pprint 
-from decouple import config
 import os
 
-
+# GLOBAL VARIABLES
+HOUR = 3600
+API_ENDPOINT = config("API_ENDPOINT")
 API_KEY = config("API_KEY")
-pp = pprint.PrettyPrinter(indent=4)
 
-url = f'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
-print(url)
-headers = {
-    'Accepts': 'application/json',
-    'X-CMC_PRO_API_KEY': API_KEY,
-}
-parameters = {
-'id':'1'
 
-}
+# Function to get the data from the api
+def getDataFeed(path):
+    url = path  # Set the path to the url
+    headers = {
+        'Accepts': 'application/json',
+        'X-CMC_PRO_API_KEY': API_KEY,   # Add the api key for permissions
+    }
+    parameters = {'id': '1'}    # Get the bitcoin data
+    session = Session()
+    session.headers.update(headers)
+    try:
+        response = session.get(url, params=parameters)
+        data = json.loads(response.text)
+        return data # Return the data object containing all of the content about bitcoin
 
-session = Session()
-session.headers.update(headers)
-try:
-    response = session.get(url, params=parameters)
-    data = json.loads(response.text)
-    pp.pprint(data)
-    
+    except (ConnectionError, Timeout, TooManyRedirects) as e:
+        data = json.loads(response.text)
+        return data # Return the error message if something goes wrong
 
-except (ConnectionError, Timeout, TooManyRedirects) as e:
-    data = json.loads(response.text)
-    pp.pprint(data)
+
+# Recursive function to get the current unix timestamp
+def getHours():
+    currentTime = int(time.time())  # Get the current time as an interger
+
+    if (currentTime % HOUR == 0):   # If the time is devisable by the number of seconds in an hour, it is a new hour
+        print(getDataFeed(API_ENDPOINT))    # Print the data form the api
+        time.sleep(1)
+        getHours()
+    else:   # If the current time is not a new hour, re run the function, adding a 1 second delay
+        time.sleep(1)
+        getHours()
+
+
+getHours()
